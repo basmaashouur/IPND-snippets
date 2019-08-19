07ca0909f8062f9f5b6ff2d3ac77c1e02058bf64
Basma Ashour <basmaashouur@gmail.com>
2019-08-19 16:33:35 +0200

    Update stringList.py

---
+++ Python/basic-syntax/stringList.py
@@ -0,0 +1,28 @@
+# strings 
+word = "hey hey there"
+## indexing
+print(word[0])
+## concatenation
+print("1" + "22")
+print("1" * 4)
+## slice strings from 1 to less than 3
+print(word[1:3])
+print(word[0:3])
+print(word[:3])
+## type convertion
+print(int('22222') + 22)
+print(str(22222) + '33')
+## join
+print(";-;".join("hello"))
+
+
+
+# lists
+
+li = ["one", "two", "three"]
+print(li[1][0])
+for x in li:
+    print(x)
+
+## join
+print(" ".join(li))
